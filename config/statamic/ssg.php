22713967ab887e2aefe488758d976fd7c2bbcb73
sdevvz <sdevvz@gmail.com>
2025-12-10 13:17:51 +0000

    final fixing

--- config/statamic/ssg.php
+++ config/statamic/ssg.php
@@ -2,53 +2,123 @@
 
 return [
 
-    'base_url' => env('APP_URL', 'http://localhost'),
+    /*
+    |--------------------------------------------------------------------------
+    | Base URL
+    |--------------------------------------------------------------------------
+    |
+    | This informs the generator where the static site will eventually be hosted.
+    | For instance, if you are relying on absolute URLs in your app, this one
+    | will be used. It should be an absolute URL, eg. "http://my-app.com"
+    |
+    */
+
+    'base_url' => config('app.url'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Destination Directory
+    |--------------------------------------------------------------------------
+    |
+    | This option defines where the static files will be saved.
+    |
+    */
 
     'destination' => storage_path('app/static'),
 
-    'copy_files' => true,
+    /*
+    |--------------------------------------------------------------------------
+    | Files and Symlinks
+    |--------------------------------------------------------------------------
+    |
+    | You are free to define a set of directories to be copied along with the
+    | generated HTML files. For example, you may want to link your CSS,
+    | JavaScript, static images, and perhaps any uploaded assets.
+    | You may choose to symlink rather than copy.
+    |
+    */
 
     'copy' => [
-        // Vite build (CRITICAL!)
         public_path('build') => 'build',
-        
-        // Favicons
-        public_path('favicon.ico') => 'favicon.ico',
-        public_path('favicon.png') => 'favicon.png',
-        
-        // SEO
-        public_path('robots.txt') => 'robots.txt',
-        public_path('sitemap.xml') => 'sitemap.xml',
-        
-        // Assets (if you have them)
+        public_path('css') => 'css',
+        public_path('js') => 'js',
         public_path('assets') => 'assets',
-        public_path('images') => 'images',
+        public_path('img') => 'img',
     ],
 
-    'exclude' => [
-        '/cp',
-        '/cp/*',
+    'symlinks' => [
+        // public_path('build') => 'build',
     ],
 
-    'glide' => false,
-
-    'workers' => 4,
+    /*
+    |--------------------------------------------------------------------------
+    | Additional URLs
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define a list of additional URLs to be generated,
+    | such as manually created routes.
+    |
+    */
 
     'urls' => [
-        '/404',
+        //
+        '/apps'
     ],
 
-    'before' => function () {
-        \Illuminate\Support\Facades\Artisan::call('cache:clear');
-    },
+    /*
+    |--------------------------------------------------------------------------
+    | Exclude URLs
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define a list of URLs that should not be generated.
+    |
+    */
 
-    'after' => function ($paths) {
-        $destination = storage_path('app/static');
-        
-        // GitHub Pages: Create .nojekyll
-        file_put_contents($destination . '/.nojekyll', '');
-        
-        \Illuminate\Support\Facades\Log::info('SSG: Generated ' . count($paths) . ' pages');
-    },
+    'exclude' => [
+        //
+        '/search'
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Route
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define how paginated entries are routed. This will take
+    | effect wherever pagination is detected in your antlers templates,
+    | like if you use the `paginate` param on the `collection` tag.
+    |
+    */
+
+    'pagination_route' => '{url}/{page_name}/{page_number}',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Glide
+    |--------------------------------------------------------------------------
+    |
+    | Glide images are dynamically resized server-side when requesting a URL.
+    | On a static site, you would just be serving HTML files without PHP.
+    | Glide images will be pre-generated into the given directory.
+    |
+    */
+
+    'glide' => [
+        'directory' => 'img',
+        'override' => true,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Failures
+    |--------------------------------------------------------------------------
+    |
+    | You may configure whether the console command will exit early with a
+    | failure status code when it encounters errors or warnings. You may
+    | want to do this to prevent deployments in CI environments, etc.
+    |
+    */
+
+    'failures' => 'warning', // 'errors' or 'warnings'
 
 ];
